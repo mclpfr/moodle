5899df9002f524556e10ca991c44e875abf56645
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-05 11:18:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 set -x
 docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
 docker exec -it moodle_moodle_1 chown bitnami:daemon /bitnami/moodle/moodledata/repository/docker
+
+BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
