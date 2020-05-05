30a53f3fd32ab7e1443b057976806b5ccac64085
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-05 11:57:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+docker-comopse up -d
 docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
 BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
 docker cp $BACKUP_MODDLE_FILE moodle_moodle_1:/bitnami/moodle/moodledata/repository/docker
