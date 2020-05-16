eb27685d48bf42812caaf93e8303da01e61e09c2
mclpfr <mc.lp.fr@gmail.com>
2020-05-16 11:50:56 +0000

    backup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -x
-docker-comopse up -d
+docker-compose up -d
 docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
 BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
 docker cp $BACKUP_MODDLE_FILE moodle_moodle_1:/bitnami/moodle/moodledata/repository/docker
 docker exec -it moodle_moodle_1 chown bitnami:daemon /bitnami/moodle/moodledata/repository/docker
+echo "Deploiement en cours ..."
+sleep 70
+echo "Deploiement terminé."
