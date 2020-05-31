138bd8e9c61675def170f096388a276e66b388f6
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 15:02:13 +0200

    Update restore_course.sh

--- restore_course.sh
+++ restore_course.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
  apt update -y
  apt install wget unzip -y
  wget https://moodle.org/plugins/download.php/21420/moosh_moodle38_2020042300.zip
@@ -7,4 +6,5 @@
  cd moosh
  ln -s $PWD/moosh.php /usr/local/bin/moosh
  cd /bitnami/moodle/moodledata/repository/docker
- moosh -n course-restore backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
+ chown bitnami:daemon ../../*
+ sudo -u bitnami moosh course-restore backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
