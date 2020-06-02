aeaa8f4a507e8f5c0b7df41170d5cd15c0d43825
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-06-02 16:39:02 +0200

    Update restore_course.sh

--- restore_course.sh
+++ restore_course.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
- apt update -y
- apt install wget unzip -y
- wget https://moodle.org/plugins/download.php/21420/moosh_moodle38_2020042300.zip
- unzip moosh_moodle38_2020042300.zip
- cd moosh
- ln -s $PWD/moosh.php /usr/local/bin/moosh
- cd /bitnami/moodle/moodledata/repository/docker
- chown bitnami:daemon ../../*
- pwd
- sudo -u bitnami moosh course-restore backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
- rm -rf /bitnami/moodle/moodledata/lock/*
+apt update -y
+apt install wget unzip -y
+wget https://moodle.org/plugins/download.php/21420/moosh_moodle38_2020042300.zip
+unzip moosh_moodle38_2020042300.zip
+cd moosh
+ln -s $PWD/moosh.php /usr/local/bin/moosh
+cd /bitnami/moodle/moodledata/repository/docker
+chown bitnami:daemon ../../*
+sudo -u bitnami moosh course-restore backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
+rm -rf /bitnami/moodle/moodledata/lock/*
+ 
+cd /bitnami/moodle/filter
+wget https://moodle.org/plugins/download.php/16971/filter_syntaxhighlighter_moodle37_2018041301.zip
+unzip filter_syntaxhighlighter_moodle37_2018041301.zip
+chown -R bitnami:daemon syntaxhighlighter
